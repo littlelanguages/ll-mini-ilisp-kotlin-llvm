29cbb175208a4e3ab57603c3f1909f8cf19c2bc5
Graeme Lockley <graeme.lockley@gmail.com>
2021-08-14 08:40:07 +0200

    feature: compile Hello 42

--- src/main/c/main.c
+++ src/main/c/main.c
@@ -6,5 +6,4 @@ extern int _main(int n);
 int main(int argc, char *argv[]) {
   _initialise_lib();
   _main(0);
-  printf("42");
 }
--- src/main/kotlin/io/littlelanguages/mil/compiler/Compiler.kt
+++ src/main/kotlin/io/littlelanguages/mil/compiler/Compiler.kt
@@ -2,13 +2,13 @@ package io.littlelanguages.mil.compiler
 
 import io.littlelanguages.data.Either
 import io.littlelanguages.data.Right
+import io.littlelanguages.mil.CompilationError
 import io.littlelanguages.mil.Errors
 import io.littlelanguages.mil.dynamic.tst.*
 import org.bytedeco.javacpp.BytePointer
-import org.bytedeco.llvm.LLVM.LLVMBuilderRef
-import org.bytedeco.llvm.LLVM.LLVMContextRef
-import org.bytedeco.llvm.LLVM.LLVMModuleRef
-import org.bytedeco.llvm.LLVM.LLVMValueRef
+import org.bytedeco.javacpp.Pointer
+import org.bytedeco.javacpp.PointerPointer
+import org.bytedeco.llvm.LLVM.*
 import org.bytedeco.llvm.global.LLVM
 
 interface LLVMState {
@@ -39,9 +39,92 @@ private class Compiler(
     override val context: LLVMContextRef,
     override val module: LLVMModuleRef
 ) : LLVMState {
+    val builder: LLVMBuilderRef = LLVM.LLVMCreateBuilderInContext(context)
+
     override val declarations = mutableMapOf<String, LLVMValueRef>()
+    var expressionName = 0
+
+    val structValue = LLVM.LLVMStructCreateNamed(context, "struct.Value")!!
+    val structValueP = LLVM.LLVMPointerType(structValue, 0)!!
+    val structValuePP = LLVM.LLVMPointerType(structValueP, 0)!!
+    val unionAnon = LLVM.LLVMStructCreateNamed(context, "union.anon")!!
+    val structPair = LLVM.LLVMStructCreateNamed(context, "struct.Pair")!!
+    val structVector = LLVM.LLVMStructCreateNamed(context, "struct.Vector")!!
+    val structNativeClosure = LLVM.LLVMStructCreateNamed(context, "struct.NativeClosure")!!
+    val structDynamicClosure = LLVM.LLVMStructCreateNamed(context, "struct.DynamicClosure")!!
+
+    val void = LLVM.LLVMVoidTypeInContext(context)!!
+    val i8 = LLVM.LLVMInt8TypeInContext(context)!!
+    val i32 = LLVM.LLVMInt32TypeInContext(context)!!
+    val i8P = LLVM.LLVMPointerType(i8, 0)!!
+
+    val _print_value = LLVM.LLVMAddFunction(module, "_print_value", LLVM.LLVMFunctionType(void, structValueP, 1, 0))
+    val _print_newline = LLVM.LLVMAddFunction(module, "_print_newline", LLVM.LLVMFunctionType(void, PointerPointer<LLVMTypeRef>(), 0, 0))
+    val _from_literal_int = LLVM.LLVMAddFunction(module, "_from_literal_int", LLVM.LLVMFunctionType(structValueP, i32, 1, 0))
+
+    init {
+        LLVM.LLVMStructSetBody(
+            structValue,
+            PointerPointer(
+                i32,
+                unionAnon
+            ),
+            2,
+            0
+        )
+
+        LLVM.LLVMStructSetBody(
+            unionAnon,
+            structNativeClosure,
+            1,
+            0
+        )
+
+        LLVM.LLVMStructSetBody(
+            structPair,
+            PointerPointer(
+                structValue,
+                structValue
+            ),
+            2,
+            0
+        )
+
+        LLVM.LLVMStructSetBody(
+            structVector,
+            PointerPointer(
+                i32,
+                structValuePP
+            ),
+            2,
+            0
+        )
+
+        LLVM.LLVMStructSetBody(
+            structNativeClosure,
+            PointerPointer(
+                i8P,
+                i32,
+                i8P
+            ),
+            3,
+            0
+        )
+
+        LLVM.LLVMStructSetBody(
+            structDynamicClosure,
+            PointerPointer(
+                i8P,
+                i32,
+                structValueP
+            ),
+            3,
+            0
+        )
+    }
 
     override fun dispose() {
+        LLVM.LLVMDisposeBuilder(builder)
         LLVM.LLVMContextDispose(context)
     }
 
@@ -52,9 +135,9 @@ private class Compiler(
 
         val error = BytePointer()
         if (LLVM.LLVMVerifyModule(module, LLVM.LLVMPrintMessageAction, error) != 0) {
-            println(error.string)
+            val message = error.string
             LLVM.LLVMDisposeMessage(error)
-            TODO("Not yet implemented")
+            throw CompilationError(message)
         }
     }
 
@@ -66,10 +149,9 @@ private class Compiler(
     }
 
     private fun compileProcedure(declaration: Procedure) {
-        val builder: LLVMBuilderRef = LLVM.LLVMCreateBuilderInContext(context)
-        val i32Type = LLVM.LLVMInt32TypeInContext(context)
-        val procedureType = LLVM.LLVMFunctionType(i32Type, i32Type,  /* argumentCount */1,  /* isVariadic */0)
+        val procedureType = LLVM.LLVMFunctionType(i32, PointerPointer<LLVMTypeRef>(), 0, 0)
         val procedure = LLVM.LLVMAddFunction(module, declaration.name, procedureType)
+
         declarations[declaration.name] = procedure
 
         LLVM.LLVMSetFunctionCallConv(procedure, LLVM.LLVMCCallConv)
@@ -77,21 +159,56 @@ private class Compiler(
         val entry = LLVM.LLVMAppendBasicBlockInContext(context, procedure, "entry")
         LLVM.LLVMPositionBuilderAtEnd(builder, entry)
 
+        expressionName = 0
         for (e in declaration.es) {
             compileE(e)
         }
-        LLVM.LLVMBuildRet(builder, LLVM.LLVMConstInt(i32Type, 0,  /* signExtend */0))
 
-        LLVM.LLVMDisposeBuilder(builder)
+//        val op = LLVM.LLVMConstStringInContext(context, BytePointer("Hello world"), 12, 0)
+//        println(op.toString())
+
+//        val v42 = LLVM.LLVMConstInt(i32, 42, 0)
+//        val o1 = LLVM.LLVMBuildCall(
+//            builder, _from_literal_int, PointerPointer<Pointer>(1)
+//                .put(0, v42), 1, "o1"
+//        )
+//        LLVM.LLVMBuildCall(builder, _print_value, PointerPointer<Pointer>(1).put(0, o1), 1, "")
+//
+//        LLVM.LLVMBuildCall(builder, _print_newline, PointerPointer<Pointer>(0), 0, "")
+
+        LLVM.LLVMBuildRet(builder, LLVM.LLVMConstInt(i32, 0, 0))
     }
 
     private fun compileE(e: Expression): LLVMValueRef? {
         when (e) {
-            is PrintlnExpression ->
+            is LiteralInt -> {
+                val v = LLVM.LLVMConstInt(i32, e.value.toLong(), 0)
+                val name = nextName()
+                return LLVM.LLVMBuildCall(builder, _from_literal_int, PointerPointer<Pointer>(1).put(0, v), 1, name)
+            }
+
+            is PrintlnExpression -> {
+                for (it in e.es) {
+                    val op = compileE(it)
+
+                    if (op != null) {
+                        LLVM.LLVMBuildCall(builder, _print_value, PointerPointer<Pointer>(1).put(0, op), 1, "")
+                    }
+                }
+
+                LLVM.LLVMBuildCall(builder, _print_newline, PointerPointer<Pointer>(0), 0, "")
+
                 return null
+            }
 
             else ->
                 TODO(e.toString())
         }
+    }
+
+    private fun nextName(): String {
+        val result = "v$expressionName"
+        expressionName += 1
+        return result
     }
 }
