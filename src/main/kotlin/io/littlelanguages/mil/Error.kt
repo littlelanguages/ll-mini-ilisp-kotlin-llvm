eeb2e5cae77311b1a46a6d6af71fd44a62a67d0f
Graeme Lockley <graeme.lockley@gmail.com>
2021-08-14 08:37:59 +0200

    feature: add compilation error

--- src/main/kotlin/io/littlelanguages/mil/Error.kt
+++ src/main/kotlin/io/littlelanguages/mil/Error.kt
@@ -4,15 +4,22 @@ import io.littlelanguages.data.Yamlable
 import io.littlelanguages.mil.static.TToken
 import io.littlelanguages.mil.static.Token
 
-sealed class Errors : Yamlable
+sealed interface Errors : Yamlable
 
 data class ParseError(
-        val found: Token,
-        val expected: Set<TToken>) : Errors() {
+    val found: Token,
+    val expected: Set<TToken>
+) : Errors {
     override fun yaml(): Any =
-            singletonMap("ParseError", mapOf(
-                    Pair("found", found),
-                    Pair("expected", expected)
-            ))
+        singletonMap(
+            "ParseError", mapOf(
+                Pair("found", found),
+                Pair("expected", expected)
+            )
+        )
 }
 
+data class CompilationError(override val message: String) : Exception(message), Errors {
+    override fun yaml(): Any =
+        singletonMap("CompilationError", message)
+}
